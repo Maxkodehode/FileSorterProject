839d1ac633ebe76484eaa0d813488b2a436d4085
Max <maxkodehode@gmail.com>
2025-11-04 13:56:36 +0100

    Added_FolderSelector_Class-File

---
+++ FolderSelector.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Diagnostics;
+
+public class FolderSelector
+{
+    public static string SelectFolder()
+{
+        var psi = new ProcessStartInfo
+        {
+            FileName = "zenity",
+            Arguments = "--file-selection --directory --title=\"Select a directory\"",
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+        };
+        
+        using (var process = Process.Start(psi))
+        {
+            string selectedPath = process.StandardOutput.ReadToEnd().Trim();  
+          process.WaitForExit();
+            return selectedPath;
+        }
+    }
+}
